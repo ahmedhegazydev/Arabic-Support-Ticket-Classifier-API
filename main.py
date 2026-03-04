78ac4096410028df22e56d5c789ae3ffc7fc6312
Ahmed Hegazy <13158523+ahmedhegazydev@users.noreply.github.com>
2026-03-04 22:12:31 +0200

    تحميل الموديل (الجزء الأهم)

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ import time
 import uuid
 
 app = FastAPI(title="Arabic Ticket Classifier API", version="0.1.0")
+
+classifier = pipeline("zero-shot-classification", model="joeddav/xlm-roberta-large-xnli")
