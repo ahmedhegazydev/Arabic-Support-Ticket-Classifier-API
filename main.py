539cfdd9ab8505e9e9f0298bd3f297d31c5b87f6
Ahmed Hegazy <13158523+ahmedhegazydev@users.noreply.github.com>
2026-03-04 22:09:57 +0200

    feat: just adding imports

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from transformers import pipeline
+import time
+import uuid
+
+app = FastAPI(title="Arabic Ticket Classifier API", version="0.1.0")
